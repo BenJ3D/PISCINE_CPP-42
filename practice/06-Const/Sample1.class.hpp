5639fdf60d471b2b50f60151174b0693fb252362
BenJ3D <benjamin@ben3d.fr>
2022-05-31 21:11:07 +0200

    save

---
+++ practice/06-Const/Sample1.class.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sample1.class.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 18:49:09 by bducrocq          #+#    #+#             */
+/*   Updated: 2022/05/31 21:08:06 by bducrocq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SAMPLE1_CLASS_H
+# define SAMPLE1_CLASS_H
+
+# include <iostream>
+
+class Sample1 {
+
+	public:
+
+	float const	pi;
+	int			qd;
+		
+		Sample1(float const f);
+		~Sample1(void);
+
+		void	bar(void) const;
+};
+
+#endif
